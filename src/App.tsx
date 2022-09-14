5f47908226275ded9abdfc307fd0e1b19d0e4640
Amr Zayed <amrzayed40@gmail.com>
2022-09-14 14:25:26 +0200

    babylon-react init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from "react";
 import './App.css';
 
+//-------------------------------------------------#
+//import the clas you are currently working on here#
+//-------------------------------------------------#
+//import { BasicScene } from "./BabylonExamples/BasicScene";
+
 function App() {
+  useEffect(() => {
+    // Update the document title using the browser API
+    const canvas = document.querySelector('canvas')!;
+
+    //create instance of the class as follows
+    //new BasicScene(canvas);
+  },[]); 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>
+        Babylon Examples
+      </h3>
+      <canvas className="babylon-canvas"></canvas>
     </div>
   );
 }
